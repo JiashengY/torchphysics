8940f4556e3d87ba96ceb0af6e4094ad12662028
JiashengY <jiasheng.yang@outlook.de>
2024-06-21 15:42:54 +0200

    update_friday_2289_2D_turbulent

--- Laminar_2D/Matlab_Data/Writedist.m
+++ Laminar_2D/Matlab_Data/Writedist.m
@@ -1,7 +1,7 @@
-load('/net/istmhome/users/hi224/Dokumente/simson_Postprocessing/ML_ks/Round_0/1113/01/xy_tot_T.mat')
+load('/net/istmhome/users/hi224/Dokumente/simson_Postprocessing/ML_ks/Round_0/2289/01/xy_tot_T.mat')
 titles=["x","dist"];
-dist_1d=dist(:,144);
+dist_1d=dist(:,1);
 datamatrix=[linspace(0,max(x),length(dist_1d))',dist_1d];
 data=[titles;datamatrix];
 %csvwrite("Flat_Turb_500.csv",data)
-writematrix(data,'DIST_1113.csv','Delimiter',',')
+writematrix(data,'DIST_2289.csv','Delimiter',',')
